f737430f59cd2ec3be35b347c2573be4f31551c7
Steven Fackler <sfackler@gmail.com>
2013-04-22 16:38:12 -0400

    Updated prompt
    Looks for three color variables:
    prompt_color_success
    prompt_color_error
    prompt_color_git

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,11 @@
 function fish_prompt --description 'Write out the prompt'
-    echo -n -s $__fish_prompt_cwd (prompt_pwd) $__fish_prompt_normal " > "
+	set -l status_color
+
+    if [ $status -eq 0 ]
+        set status_color $prompt_color_success
+    else
+        set status_color $prompt_color_error
+    end
+
+    echo -n -s (set_color $status_color) (prompt_pwd) " ❯ "
 end
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -3,6 +3,6 @@ function fish_right_prompt --description 'Write out the right prompt'
 	set -g __fish_git_prompt_showstashstate 1
 	set -g __fish_git_prompt_showuntrackedfiles 1
 	set -g __fish_git_prompt_showupstream auto
-	set -g __fish_git_prompt_color green
-	__fish_git_prompt "[%s]"
+	set -g __fish_git_prompt_color $prompt_color_git
+	echo -n (set_color $fish_color_normal) (__fish_git_prompt "[%s]")
 end

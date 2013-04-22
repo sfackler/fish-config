b4a3a65dade6872b2b2f678bb02f172af1986e39
Steven Fackler <sfackler@gmail.com>
2013-04-22 17:02:19 -0400

    Default values for prompt colors

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,7 +1,17 @@
 function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
+
+	if not set -q prompt_color_success
+        set -U prompt_color_success 5FAF5F --bold
+    end
+
+    if not set -q prompt_color_error
+        set -U prompt_color_error AF0000 --bold
+    end
+
 	set -l status_color
 
-    if [ $status -eq 0 ]
+    if [ $last_status -eq 0 ]
         set status_color $prompt_color_success
     else
         set status_color $prompt_color_error
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,4 +1,8 @@
 function fish_right_prompt --description 'Write out the right prompt'
+	if not set -q prompt_color_git
+        set -U prompt_color_git 6C6C6C
+    end
+
 	set -g __fish_git_prompt_showdirtystate 1
 	set -g __fish_git_prompt_showstashstate 1
 	set -g __fish_git_prompt_showuntrackedfiles 1
